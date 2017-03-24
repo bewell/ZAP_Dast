9d3e8029fdf8dfe9bae445ab8fda5d0247ab575e
bewell <punk@bewell>
2017-03-24 11:44:56 +0200

    message

--- scan.py
+++ scan.py
@@ -4,12 +4,20 @@ import time
 from pprint import pprint
 from zapv2 import ZAPv2
 import params
+import subprocess
 
 ############## This is the place for parameters #################
 target = params.target
 apikey = params.apikey
 zap = params.zap
+logfile_zap = params.logfile_zap
+logfile_alerts = params.logfile_alerts
 ##################### / ###########################
+
+print 'Launching the ZAP proxy. Wait a few seconds'
+#this launching process is on Mac. If you're using on Windows, please modify the path to the ZAP executable
+subprocess.Popen('/Applications/OWASP\ ZAP.app/Contents/MacOS/OWASP\ ZAP.sh %s' % (str('-daemon')), shell=True, stdout=logfile_zap) 
+time.sleep(10)
 
 # do stuff
 print 'Accessing target %s' % target
@@ -40,6 +48,11 @@ print 'Scan completed'
 
 # Report the results
 
-print 'Hosts: ' + ', '.join(zap.core.hosts)
-print 'Alerts: '
-pprint (zap.core.alerts())
+print 'Host that was scanned: ' + ', '.join(zap.core.hosts)
+print 'Alerts can be found in this file: ' + str(logfile_alerts)
+pprint (zap.core.alerts(), logfile_alerts)
+
+# Shuttting down ZAP scanner
+print 'Shutting down ZAP Scanner, wait 10s'
+zap.core.shutdown(apikey=apikey)
+time.sleep(10)
