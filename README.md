7472ea0d5aa11df95e3975c7204d48bcc1c7e444
bewell <punk@bewell>
2017-03-27 10:24:11 +0300

    message

--- README.md
+++ README.md
@@ -3,18 +3,12 @@
 Controlling DAST scanner via the script is quite usefull, since you can call it right after your unit tests or in any other situation where manual interaction is not prefered. 
 This python script will help you achieve this, by the way, you are welcome to change the script as you like. 
 
-You will need the ZAPv2 library as a prerequisite. It's a library to interact with the ZAP api. The documentation about it can be found here: [ZapV2](https://github.com/zaproxy/zap-api-python/tree/master/src/zapv2)
 
 ## Steps to successfully launch the scan
 
-  - Launch ZAP scanner somewhere (it doesn't even need to be on your machine)
-  - Have in mind the IP address where the ZAP is running and the port on which it listens
-  - Open the `params.py` file and edit the `zap` variable. You will find the syntax inside the file
-  - On ZAP again and navigate to the `Preferences`, pick the `API` node
-  - Your value of interest is the `API Key`. It looks something like this: `olthv41fjb67953u863ddaj6ej`. 
-  - This key has to go to the `params.py` file as well. There's a variable called `apikey`.
-  - In the same `params.py` define what is your target
-  - Now you can launch the script by easily typing
-    ```
-    python scan.py
+  - Place ZAP scanner somewhere (https://github.com/zaproxy/zaproxy/releases/download/2.5.0/ZAP_2.5.0_Cross_Platform.zip - Consult PLC)
+  - Open the `scan.py` file and edit the `startZapPath` variable. You will find the syntax inside the file
+  - Navigate to the root folder and launch the script
+    ```sh
+    $ python scan.py http://127.0.0.1:8080
     ```
--- scan.py
+++ scan.py
@@ -1,58 +1,78 @@
 #!/usr/bin/env python
+import sys
+sys.path.append("zapv2")
 
+import json
 import time
 from pprint import pprint
 from zapv2 import ZAPv2
-import params
 import subprocess
 
-############## This is the place for parameters #################
-target = params.target
-apikey = params.apikey
-zap = params.zap
-logfile_zap = params.logfile_zap
-logfile_alerts = params.logfile_alerts
+############## parameters #################
+startZapPath = '/Users/g01059639/software/ZAP_2.5.0/zap.sh -daemon -port 9090 -config api.disablekey=true'
+target = sys.argv[1]
+zap = ZAPv2(proxies={'http': 'http://127.0.0.1:9090', 'https': 'http://127.0.0.1:9090'})
+logfile_zap = open('logfile_zap.log', 'w')
+logfile_alerts = open('logfile_alerts.log', 'w')
 ##################### / ###########################
 
-print 'Launching the ZAP proxy. Wait a few seconds'
-#this launching process is on Mac. If you're using on Windows, please modify the path to the ZAP executable
-subprocess.Popen('/Applications/OWASP\ ZAP.app/Contents/MacOS/OWASP\ ZAP.sh %s' % (str('-daemon')), shell=True, stdout=logfile_zap) 
+print 'Launching the ZAP. Wait a few seconds'
+subprocess.Popen( startZapPath, shell=True, stdout=logfile_zap)
 time.sleep(10)
 
-# do stuff
 print 'Accessing target %s' % target
-# try have a unique enough session...
 zap.urlopen(target)
-# Give the sites tree a chance to get updated
-time.sleep(2)
+time.sleep(3)
 
 print 'Spidering target %s' % target
-scanid = zap.spider.scan(target, apikey=apikey)
+scanid = zap.spider.scan(target)
 # Give the Spider a chance to start
-time.sleep(2)
-while (int(zap.spider.status(scanid)) < 100):
-    print 'Spider progress %: ' + zap.spider.status(scanid)
-    time.sleep(2)
+time.sleep(3)
+try:
+    while (int(zap.spider.status(scanid)) < 100):
+        print 'Spider progress %: ' + zap.spider.status(scanid)
+        time.sleep(0.5)
+except:
+    print "Oops"
+    zap.core.shutdown()
+    sys.exit(0)
 
 print 'Spider completed'
 # Give the passive scanner a chance to finish
-time.sleep(5)
+time.sleep(3)
+
+
 
 print 'Scanning target %s' % target
-scanid = zap.ascan.scan(target, apikey=apikey)
+scanid = zap.ascan.scan(target)
 while (int(zap.ascan.status(scanid)) < 100):
     print 'Scan progress %: ' + zap.ascan.status(scanid)
-    time.sleep(5)
+    time.sleep(0.5)
 
 print 'Scan completed'
+
+
 
 # Report the results
 
 print 'Host that was scanned: ' + ', '.join(zap.core.hosts)
 print 'Alerts can be found in this file: ' + str(logfile_alerts)
-pprint (zap.core.alerts(), logfile_alerts)
 
-# Shuttting down ZAP scanner
-print 'Shutting down ZAP Scanner, wait 10s'
-zap.core.shutdown(apikey=apikey)
-time.sleep(10)
+
+jsonOutput = zap.core.alerts()
+jsonOutputString = json.dumps(jsonOutput)
+
+try:
+    #print jsonOutput[0]["name"]
+    for i in jsonOutput:
+        print i["risk"] + " -- " + i["url"] + " -- " + i["name"]
+
+    pprint (jsonOutputString, logfile_alerts)
+except:
+    print "Oops!"
+
+
+
+print 'Shutting down ZAP Scanner, wait 4s'
+zap.core.shutdown()
+time.sleep(4)
